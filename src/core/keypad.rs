5d2873d191933d33019f18859ab0f93e36df1e0d
keelus <hugomoreda@hotmail.com>
2024-07-24 20:39:28 +0200

    Added a basic implementation of the Keypad.

--- src/core/keypad.rs
+++ src/core/keypad.rs
@@ -1,7 +1,39 @@
-pub struct Keypad {}
+use std::collections::HashMap;
+
+// Keypad:
+// 1 2 3 C    1 2 3 4
+// 4 5 6 D -> Q W E R
+// 7 8 9 E -> A S D F
+// A 0 B F    Z X C V
+//
+pub struct Keypad {
+    key_map: HashMap<u8, bool>,
+    last_key: Option<u8>,
+}
 
 impl Keypad {
     pub fn new() -> Keypad {
-        Keypad {}
+        Keypad {
+            key_map: HashMap::from((0x0..=0xF).map(|i| (i, false)).collect::<HashMap<_, _>>()),
+            last_key: None,
+        }
+    }
+
+    pub fn set_key(&mut self, idx: u8, state: bool) {
+        let entry = self.key_map.get_mut(&idx).unwrap();
+
+        if *entry && !state {
+            // Key released
+            self.last_key = Some(idx);
+        }
+
+        *entry = state;
+    }
+
+    // Note: get_released_key() consumes the last_key (if any)
+    pub fn get_released_key(&mut self) -> Option<u8> {
+        let last_key = self.last_key;
+        self.last_key = None;
+        last_key
     }
 }
